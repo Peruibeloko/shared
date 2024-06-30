ff9c53dd07c1dbf3c85d9157739fb9ffce5b6a2b
peruibeloko <cmendonca32@gmail.com>
2024-06-29 22:14:29 -0300

    web components are cool

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# shared
+# Shared
 
 This repo contains shared workflows, stylesheets, and config files across my different projects
---
+++ components/CodeSnippet/CodeSnippet.js
@@ -0,0 +1,42 @@
+/// @ts-check
+
+class CodeSnippet extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  async connectedCallback() {
+    //TODO fetch template and add to shadow root
+    const shadow = this.attachShadow({ mode: 'open' });
+
+    const parser = new DOMParser();
+    const templateContents = await fetch('./components/CodeSnippet/template.html').then(d => d.text());
+    console.log(templateContents);
+    const template = parser.parseFromString(templateContents, 'text/html');
+
+    // fetch source file and add it to inner code element
+    const fileName = this.getAttribute('src');
+    if (!fileName) return;
+
+    const sourceCode = await fetch(fileName).then(d => d.text());
+
+    const codeBlock = template.querySelector('code');
+    if (!codeBlock) return;
+
+    codeBlock.innerText = sourceCode;
+
+    // register click handler
+
+    const copyButton = template.querySelector('button');
+
+    copyButton?.addEventListener('click', () => {
+      navigator.clipboard.writeText(sourceCode);
+    });
+
+    for (const child of template.children) {
+      shadow.appendChild(child);
+    }
+  }
+}
+
+customElements.define('code-snippet', CodeSnippet);
